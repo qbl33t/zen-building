513e2d3664ab5839d321522f1f46858942d14cca
qbl33t <qbl33t@gmail.com>
2024-08-03 17:32:10 +0200

    forget add all new stuff - materials etc.ˆ

---
+++ Assets/Scripts/Logic/CubeDetectorLogic.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Logic {
+	public class CubeDetectorLogic : MonoBehaviour {
+		// Start is called once before the first execution of Update after the MonoBehaviour is created
+		private void Start() {
+		}
+
+		// Update is called once per frame
+		private void Update() {
+			Debug.Log("test log");
+		}
+	}
+}
